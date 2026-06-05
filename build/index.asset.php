88215ca80a47d52b1e77628e1f205dcd52f5f618
emontecer <emontecer@gmail.com>
2026-06-05 12:36:04 +0200

    Refactor code structure for improved readability and maintainability

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd02d964b4e352d59a64d');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '30118ff7d0e0d157020c');
